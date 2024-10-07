138d232bff886debed479a48281ac47dbcd5a86d
Lucas Mösch <lucasmoesch@live.de>
2024-10-08 00:00:57 +0200

    initial commit

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/native-federation';
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'flights'},
+    {
+        path: 'flights',
+        loadComponent: () =>
+        loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+    },
+];
