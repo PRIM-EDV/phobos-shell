5f0c01a4e05010c76f406333f65640a5a5ab26e3
Lucas Mösch <lucasmoesch@live.de>
2025-05-24 11:37:44 +0200

    Fixed broken legacy ph-elements imports

--- frontend/src/app/shell/shell.component.ts
+++ frontend/src/app/shell/shell.component.ts
@@ -1,51 +1,45 @@
-import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
-import { PhElementsModule } from '../../../lib/ph-elements/ph-elements.module';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { Component, computed, Signal, signal, WritableSignal } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { PhSidebar, PhSidebarItem, PhTopbar, PhTopbarHeader, PhTopbarItem } from "@phobos/elements";
+
+import { filter } from "rxjs";
 
 @Component({
-  selector: 'app-shell',
-  imports: [
-    PhElementsModule
-  ],
+  selector: "app-shell",
+  imports: [PhTopbar, PhTopbarHeader, PhTopbarItem, PhSidebar, PhSidebarItem],
   standalone: true,
-  templateUrl: './shell.component.html',
-  styleUrl: './shell.component.scss'
+  templateUrl: "./shell.component.html",
+  styleUrl: "./shell.component.scss",
 })
 export class ShellComponent {
-
   public view: Signal<string> = computed(() => {
     const url = this.currentUrl();
-    const parts = url.split('/').filter(Boolean);
-   switch(parts[0]) {
-      case 'maptool':
-        return 'TACOP';
+    const parts = url.split("/").filter(Boolean);
+    switch (parts[0]) {
+      case "maptool":
+        return "TACOP";
       default:
-        return '';
-   }
+        return "";
+    }
   });
 
   public tab: Signal<string> = computed(() => {
     const url = this.currentUrl();
-    const parts = url.split('/').filter(Boolean);
-    switch(parts[1]) {
-        case 'map':
-          return 'MAP';
-        case 'squad':
-          return 'SQUAD';
-        default:
-          return '';
+    const parts = url.split("/").filter(Boolean);
+    switch (parts[1]) {
+      case "map":
+        return "MAP";
+      case "squad":
+        return "SQUAD";
+      default:
+        return "";
     }
-  })
+  });
 
-  private currentUrl: WritableSignal<string> = signal('');
-
-
+  private currentUrl: WritableSignal<string> = signal("");
 
   constructor(private readonly router: Router) {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(event => {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event) => {
       this.currentUrl.set((event as NavigationEnd).urlAfterRedirects);
     });
   }
