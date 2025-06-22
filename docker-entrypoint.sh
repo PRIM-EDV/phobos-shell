f454663f620635d7664c684b8370236419e5c4a0
Lucas Mösch <lucasmoesch@live.de>
2025-06-22 14:19:18 +0200

    Fixed Dockerfile build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,2 @@
-db = db.getSiblingDB('nest');
-
-db.users.insertOne({
-  username: "admin",
-  password: "$argon2id$v=19$m=65536,t=3,p=4$eTwQqQU+N5EO+VtNXLj3gA$TVPl1R91M+lI2djVCULbpScc1HSTukoswlSKG/X8Sfs",
-  role: "admin"
-});
+#!/bin/sh
+cd "/opt/phobos-shell/apps/backend" && npm run start
