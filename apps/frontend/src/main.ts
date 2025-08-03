fcbceb16cf3f3110b2a49cfadb1f93cca859ac0e
Lucas Mösch <lucasmoesch@live.de>
2025-08-03 17:12:31 +0200

    Added fixes for angular v20

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { initFederation } from '@angular-architects/native-federation';
 
+// Bugfix for native-federation
+(window as any).ngDevMode = false;
+
 initFederation('federation.manifest.json')
   .catch(err => console.error(err))
   .then(_ => import('./bootstrap'))
