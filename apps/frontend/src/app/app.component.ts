092dd86e519f8320584a02cf067e87a5529a6fcf
Lucas Mösch <lucasmoesch@live.de>
2025-08-04 18:16:04 +0200

    Added preliminary reroute

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,33 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AuthService } from './auth/auth.service';
+import { Component } from "@angular/core";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
 import { ShellComponent } from "./shell/shell.component";
+import { filter } from "rxjs";
+import { AuthzService } from "./auth/authz.service";
 // import { EventEmitter2 } from 'eventemitter2';
 
 declare global {
   interface Window {
     __env?: {
-      phobosAuthUrl?: string,
-    }
+      phobosAuthUrl?: string;
+    };
   }
 }
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [
-    RouterOutlet, 
-    ShellComponent
-  ],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, ShellComponent],
+  templateUrl: "./app.component.html",
   styles: [],
 })
 export class AppComponent {
-  title = 'phobos-shell';
+  title = "phobos-shell";
 
   constructor(
-    private readonly auth: AuthService
-  ) {
-    
+    private readonly authz: AuthzService,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit() {
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(async (e: NavigationEnd) => {
+      if (e.urlAfterRedirects === "/") {
+        await this.redirectToUserLanding();
+      }
+    });
+  }
+
+  private async redirectToUserLanding(): Promise<void> {
+    switch (true) {
+      case await this.authz.hasRole('admin'):
+        await this.router.navigate(['/maptool/map']);
+    }
   }
 }
