359ce8ff28a01ace6f78727596b43d31ef538154
Lucas Mösch <lucasmoesch@live.de>
2025-08-07 08:23:44 +0200

    Removed error component

--- apps/frontend/src/app/infrastructure/ui/error/error.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-error",
-  templateUrl: "./error.component.html",
-  styleUrls: ["./error.component.css"],
-  standalone: true
-})
-export class ErrorComponent {}
