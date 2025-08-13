f996a7f889e124c2a597c270ce0cd42ff81ec973
Lucas Mösch <lucasmoesch@live.de>
2025-08-13 10:25:22 +0200

    Added more documentation to docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,6 @@
 # Generate web environment
 envsubst < /opt/phobos-shell/apps/frontend/public/federation.manifest.template.json > /opt/phobos-shell/apps/frontend/public/federation.manifest.json
 envsubst < /opt/phobos-shell/apps/frontend/public/js/env.template.js > /opt/phobos-shell/apps/frontend/public/js/env.js
+
+# Start the application
 cd "/opt/phobos-shell/apps/backend" && npm run start
